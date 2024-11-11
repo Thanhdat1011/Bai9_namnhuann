eae0f6c04a3b6fb5ea8cbc9073e8037db94783b0
MAY 06 <MAY 06@LAB2-06>
2024-11-11 16:27:41 +0700

    Add project files.

---
+++ Bai9_namnhuann.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+bool isNamNhuan(int sonam);
+int main()
+{
+
+	int nam;
+	cout << "nhap nam: ";
+	cin >> nam;
+
+	if (isNamNhuan(nam))
+	{
+		cout << "Nam Nhuan";
+	}
+	else {
+		cout << "Nam Ko nhuan";
+	}
+	return 0;
+}
+/// <summary>
+/// Ham dung de xac dinh nam nhuan hay ko
+/// Neu nhuan = tru
+/// Neu ko nhuan = false
+/// </summary>
+/// <param name="sonam"></param>
+/// <returns></returns>
+bool isNamNhuan(int sonam) {
+	if (sonam % 4 == 0 && sonam % 100 != 0 || sonam % 400 == 0)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+ 
